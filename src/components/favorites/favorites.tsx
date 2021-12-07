9731811809e5c926e17e67f128d94df983d2c9f5
yaroslav grigorev <yaroslav.purple@gmail.com>
2021-12-07 18:52:47 +0300

    header component; fixes

--- src/components/favorites/favorites.tsx
+++ src/components/favorites/favorites.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {STYLES_OFFERS_CARDS} from '../../const/const';
 import {Offer} from '../../mocks/offers';
 import OfferCardList from '../offer-card-list/offer-card-list';
+import Header from '../header/header';
 
 interface favoritesProps {
   offers: Offer[]
@@ -10,37 +11,42 @@ interface favoritesProps {
 const Favorites = ({offers}: favoritesProps): JSX.Element => {
   return (
     <React.Fragment>
-      <div className="page__favorites-container container">
-        <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
-          <ul className="favorites__list">
-            <li className="favorites__locations-items">
-              <div className="favorites__locations locations locations--current">
-                <div className="locations__item">
-                  <a className="locations__item-link" href="#">
-                    <span>Amsterdam</span>
-                  </a>
-                </div>
-              </div>
-              <div className="favorites__places">
-                <OfferCardList offers={offers.slice(0, 3)} style={STYLES_OFFERS_CARDS.favorites}/>
-              </div>
-            </li>
+      <div className="page">
+        <Header/>
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                <li className="favorites__locations-items">
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="#">
+                        <span>Amsterdam</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="favorites__places">
+                    <OfferCardList offers={offers.slice(0, 3)} style={STYLES_OFFERS_CARDS.favorites}/>
+                  </div>
+                </li>
 
-            <li className="favorites__locations-items">
-              <div className="favorites__locations locations locations--current">
-                <div className="locations__item">
-                  <a className="locations__item-link" href="#">
-                    <span>Cologne</span>
-                  </a>
-                </div>
-              </div>
-              <div className="favorites__places">
-                <OfferCardList offers={offers.slice(3, 6)} style={STYLES_OFFERS_CARDS.favorites}/>
-              </div>
-            </li>
-          </ul>
-        </section>
+                <li className="favorites__locations-items">
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="#">
+                        <span>Cologne</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="favorites__places">
+                    <OfferCardList offers={offers.slice(3, 6)} style={STYLES_OFFERS_CARDS.favorites}/>
+                  </div>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </main>
       </div>
     </React.Fragment>
   );
--- src/components/not-found/not-found.tsx
+++ src/components/not-found/not-found.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
+import Header from '../header/header';
 
 const NotFound = (): JSX.Element => {
   return (
     <React.Fragment>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-      <h2 style={{textAlign: `center`}}>404 Page not found</h2>
+      <div className="page page--gray page--main">
+        <Header/>
+        <h2 style={{textAlign: `center`}}>404 Page not found</h2>
+      </div>
     </React.Fragment>
   );
 };
